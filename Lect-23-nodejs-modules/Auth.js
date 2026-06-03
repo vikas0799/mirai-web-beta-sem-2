aa464c04651b1f4afa286ae4f781dc27ed5a2d42
Vikas Patel <mirai@Johns-MacBook-Air.local>
2026-06-03 09:55:05 +0530

    modules

---
+++ Lect-23-nodejs-modules/Auth.js
@@ -0,0 +1,15 @@
+let login=true;
+
+
+let prince="chicken party";
+
+
+let Auth=()=>{
+    console.log("authentication success");
+    
+}
+
+
+console.log(login);
+
+module.exports={login,prince,Auth};
---
+++ Lect-23-nodejs-modules/main.js
@@ -0,0 +1,9 @@
+console.log("hii beta batch");
+   let subhas=require("./Auth.js")
+console.log("bye");
+console.log(subhas);
+
+subhas.Auth();
+
+let operating=require("os");
+console.log(operating);
